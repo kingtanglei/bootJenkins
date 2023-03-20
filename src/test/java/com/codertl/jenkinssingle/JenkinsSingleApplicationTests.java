9c10ce101c510321cc1a1d4836fc502bada6fc04
codertl <956847690@qq.com>
2023-03-20 19:49:26 +0800

    提交

---
+++ src/test/java/com/codertl/jenkinssingle/JenkinsSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codertl.jenkinssingle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSingleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
